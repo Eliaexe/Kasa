c815e732a2c133f93b5f1373766dd3e6483cfbbe
Elia Sidori <eliasidori@outlook.it>
2023-04-07 14:58:56 +0200

    dropdown logic and css now ok

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,37 +1,62 @@
+import React, { useState } from 'react';
 import "../styles/Dropdown.css"
 
 function Dropdown(prop) {
-    const url = window.location.pathname;
+  const [isContentVisible, setIsContentVisible] = useState(true);
 
-    let content 
-    if (typeof prop.content == 'string') {
-        content = <p>{prop.content}</p>
-    } else if(typeof prop.content == 'object'){
-        content = <ul>{prop.content.map( x => <li>{x}</li>)}</ul>         
-    }
+  const showHideHandler = () => {
+    setIsContentVisible(!isContentVisible);
+  };
 
-    if (url.includes('logement')) {
-        return(<div className="drop-down-logement">
-                <div className="head-drop">
-                    <h3>{prop.title}</h3>
-                    <img className="" src="/images/arrow-right.png" alt="arrow right" />
-                </div>
-                <div className="body-drop bd-drop-height">
-                    {content}
-                </div>
-            </div>)
-    } else if (url === '/a-propos') {
-        return(<div className="drop-down-apropos">
-                <div className="head-drop">
-                    <h3>{prop.title}</h3>
-                    <img className="" src="/images/arrow-right.png" alt="arrow right" />
-                </div>
-                <div className="body-drop bd-drop-height-apopos">
-                    {content}
-                </div>
-            </div>)
-    }
-    
+  let content;
+  if (typeof prop.content === 'string') {
+    content = <p>{prop.content}</p>;
+  } else if (typeof prop.content === 'object') {
+    content = (
+      <ul>
+        {prop.content.map((x, i) => (
+          <li key={i}>{x}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  const url = window.location.pathname;
+  if (url.includes('logement')) {
+    return (
+      <div className="drop-down-logement">
+        <div className={`head-drop ${isContentVisible ? '' : 'border-radius'}`}>
+          <h3>{prop.title}</h3>
+          <img
+            className={isContentVisible ? '' : 'rotate'}
+            src="/images/arrow-right.png"
+            alt="arrow"
+            onClick={showHideHandler}
+          />
+        </div>
+        <div className={`body-drop-logement bd-drop-height ${isContentVisible ? 'd-block' : 'd-none'}`} id={prop.title}>
+          {content}
+        </div>
+      </div>
+    );
+  } else if (url === '/a-propos') {
+    return (
+      <div className="drop-down-apropos">
+        <div className={`head-drop ${isContentVisible ? '' : 'border-radius'}`}>
+          <h3 className="head-drop-title">{prop.title}</h3>
+          <img
+            className={isContentVisible ? '' : 'rotate'}
+            src="/images/arrow-right.png"
+            alt="arrow"
+            onClick={showHideHandler}
+          />
+        </div>
+        <div className={`body-drop bd-drop-height-apopos ${isContentVisible ? 'd-block' : 'd-none'}`} id={prop.title}>
+          {content}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Dropdown
+export default Dropdown;
