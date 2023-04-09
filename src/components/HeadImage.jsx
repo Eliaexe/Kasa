078300f0289fe3b2469ef7e3d5f212b3ae21a0c8
Elia Sidori <eliasidori@outlook.it>
2023-04-09 16:23:27 +0200

    homepage mediaquery for smartphone is ok, need to finish location details media query. sending message to mentor

--- src/components/HeadImage.jsx
+++ src/components/HeadImage.jsx
@@ -1,13 +1,13 @@
 import "../styles/HeadImage.css"
 
-function HeadImage(params) {
+function HeadImage() {
     return (
-<div class="head-image-container">
-  <img class="head-image" src="/images/headimage-1.png" alt="Kasa Logo" />
-  <div class="head-image-overlay">
-    <p>Chez vous, partout et ailleurs</p>
-  </div>
-</div>)
+    <div class="head-image-container">
+      <img class="head-image" src="/images/headimage-1.png" alt="Kasa Logo" />
+      <div class="head-image-overlay">
+        <p>Chez vous, partout et ailleurs</p>
+      </div>
+    </div>)
 }
 
 export default HeadImage
