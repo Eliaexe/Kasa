35fc225c04ee08cc75617951ab8115e4a43ebd1e
Elia Sidori <eliasidori@outlook.it>
2023-04-10 15:47:52 +0200

    mediaquery of all pages done, need to verify the project requirement and control if it fit. I think is ready to the delivery

--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -5,21 +5,21 @@ function LocationDetails(prop) {
     console.log(5 - prop.rating);
     return(
     <div className="location-details">
-        <div className="top-section">
+        <section className="section">
+        <div className="left-section">
             <div className="home-presentation">
                 <h1>{prop.title}</h1>
                 <h2>{prop.location}</h2>
             </div>
+            <div className="tagContainer">
+                    {prop.tags.map(tag => (<span className="tag">{tag}</span>))}
+            </div>
+        </div>
+        <div className="right-section">
             <div className="host-details">
                 <h2>{prop.host.name}</h2>
                 <img className="" src={prop.host.picture} alt="" />
             </div>
-        </div>
-        <div className="bottom-section">
-            <div className="tagContainer">
-                    {prop.tags.map(tag => (<span className="tag">{tag}</span>))}
-            </div>
-            
             <div className="star-container">
                 {Array.from({ length: prop.rating }, () => (
                     <img src="/images/Full-Star.png" alt="full star" />
@@ -29,6 +29,8 @@ function LocationDetails(prop) {
                 ))}
             </div>
         </div>
+        </section>
+        
         <div className="drop-container">
             <Dropdown title="Description" content={prop.description} />
             <Dropdown title="Equipments" content={prop.equipments} />
